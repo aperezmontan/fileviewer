8fd9346b563ff196a20b731d1a6762ae60e9ac15
Aristides Perez <aris.a.perez@gmail.com>
2017-09-29 15:52:23 -0700

    Add request specs for Links

---
+++ spec/requests/links_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe LinksController, type: :request do
+  context "when viewing a link" do
+
+    subject do
+      get "links/#{link_uid}"
+    end
+
+    context "when the link is recognized" do
+      let(:link_uid) { link.uid }
+
+      it "navigates to the page" do
+        expect(subject).to render_template("links/show")
+      end
+    end
+
+    context "when the link is not recognized" do
+      let(:link_uid) { "a" }
+
+      it "return a 404" do
+        expect(subject.status).to eq(404)
+        expect(subject.body).to eq("Could not find link with id=a")
+        expect(subject).to_not render_template("links/show")
+      end
+    end
+  end
+end
